b8f537d6e327dbf746b23c55e51f4fb65a96731c
lukaszkus <kusiu@vp.pl>
2021-10-26 11:16:06 +0200

    main section update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const switchTheme = document.querySelector('input[name=theme]');
+const themeMode = document.querySelector('.theme__mode');
 
 switchTheme.addEventListener('change', function(){
   if(this.checked){
     document.documentElement.setAttribute('data-theme', 'dark');
+    themeMode.innerHTML = "Light Mode";
+
   }else {
     document.documentElement.setAttribute('data-theme', 'light');
+    themeMode.innerHTML = "Dark Mode";
   }
 });
