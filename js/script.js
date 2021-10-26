24b54000854e6be96c21d5ca4151e58be33dc700
lukaszkus <kusiu@vp.pl>
2021-10-26 22:22:27 +0200

    desktop layout finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const switchTheme = document.querySelector('input[name=theme]');
 const themeMode = document.querySelector('.theme__mode');
 
+let modeTransition = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition')
+  }, 1000)
+}
+
 switchTheme.addEventListener('change', function(){
-  if(this.checked){
+  if (this.checked) {
+    modeTransition();
     document.documentElement.setAttribute('data-theme', 'dark');
     themeMode.innerHTML = "Light Mode";
 
-  }else {
+  } else {
+    modeTransition();
     document.documentElement.setAttribute('data-theme', 'light');
     themeMode.innerHTML = "Dark Mode";
   }
